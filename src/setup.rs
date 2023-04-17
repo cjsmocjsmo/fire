d86372237de65a92d18db0165255d04ee9c71f85
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-04-17 09:08:51 -0700

    update

--- src/setup.rs
+++ src/setup.rs
@@ -13,29 +13,7 @@ pub fn run_setup() -> bool {
         env_result = true;
     }
 
-    // let homedir = fire_walk_dirs::home_dir();
-    // println!("this is home dir {}", homedir.clone());
-    
-    // let music_dir = homedir.clone() + "/Music";
-    // let music_list = fire_walk_dirs::walk_music_dir_music(music_dir);
-
-    // let video_dir = homedir.clone() + "/Videos";
-    // let video_list = fire_walk_dirs::walk_video_dir(video_dir);
-    
-    // // walk FIRE_ADD_VIDEO_PATH
-    // let add_vids = env::var("FIRE_ADD_VIDEO_PATH").expect("$FIRE_ADD_VIDEO_PATH is not set");
-    // let nonE = String::from("NONE");
-    // if add_vids != nonE {
-    //     let vlist2 = fire_walk_dirs::walk_music_dir_music(add_vids);
-    // }
-    
-
-    // // walk FIRE_ADD_MUSIC_PATH
-    // let add_music = env::var("FIRE_ADD_MUSIC_PATH").expect("$FIRE_ADD_MUSIC_PATH is not set");
-    // let mlist2 = fire_walk_dirs::walk_music_dir_music(add_music);
-
-    // // scan posters2 
-    // // scan mp3 folders for images
+    fire_walk_dirs::run_all_walkers();
 
     let mut result = false;
 
