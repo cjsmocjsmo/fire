200abfc358abf51393cb39eb09b0df7510d24ffd
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-04-24 14:04:32 -0700

    add timing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::time::{Duration, Instant};
+
 pub mod setup;
 
 fn main() {
+    let start = Instant::now();
     let setup_result = setup::run_setup();
-    println!("\nSetup is Complete! {}", setup_result);
+    let duration = start.elapsed();
+    println!("\nSetup is Complete! {:?} {}", duration, setup_result);
 }
