e69b4aa80fa8e2d4b6731997d327623590f5406a
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-04-25 05:55:42 -0700

    test

--- src/setup/fire_process_music_images.rs
+++ src/setup/fire_process_music_images.rs
@@ -17,15 +17,24 @@ fn create_music_thumbnail(x: &String, art: String, alb: String) -> String{
     out_fname.to_string()
 }
 
+#[derive(Debug)]
+struct MusicImageInfo {
+    id: String,
+    width: String,
+    height: String,
+    basedir: String,
+    filename: String,
+    extension: String,
+    artist: String, 
+    album: String,
+    filesize: String,
+    fullpath: String,
+    thumbpath: String,
+
+}
+
 pub fn process_music_images(x: String, index: i32) -> String {
-    // let mp3_imagesvec = crate::setup::fire_walk_dirs::walk_music_dir_images();
 
-    // let mut image_count = 0;
-
-    // let mut bad_image_vec = vec![];
-
-    // for jpg in mp3_imagesvec {
-        
 
         let id = crate::setup::fire_misc::get_md5(&x);
 
@@ -40,37 +49,50 @@ pub fn process_music_images(x: String, index: i32) -> String {
             let base_dir = crate::setup::fire_split::split_base_dir(&x);
             
             let file_name = crate::setup::fire_split::split_filename(&x);
-            let extension = crate::setup::fire_split::split_ext(&x);
-            
-
+            let ext = crate::setup::fire_split::split_ext(&x);
 
             let artist_results = crate::setup::fire_split::image_split_artist(&base_dir);
             let album_results = crate::setup::fire_split::image_split_album(&base_dir);
 
-            println!("\n{}\n\t{}\n\t{}\n\t{}", x.clone(), artist_results, album_results, extension);
+            // println!("\n{}\n\t{}\n\t{}\n\t{}", x.clone(), artist_results, album_results, extension);
 
-        //     let fsize_results = crate::setup::fire_misc::get_file_size(&x).to_string();
-        //     let fullpath = &x.to_string();
+            let fsize_results = crate::setup::fire_misc::get_file_size(&x).to_string();
+            let full_path = &x.to_string();
 
-        //     let thumb_path = create_music_thumbnail(&x, artist_results.clone(), album_results.clone());
+            let thumb_path = create_music_thumbnail(&x, artist_results.clone(), album_results.clone());
 
-        //     // let b64image = crate::setup::fire_image::to_base64_str(&thumb_path);
+            // let b64image = crate::setup::fire_image::to_base64_str(&thumb_path);
 
-        //     crate::setup::fire_image::write_image_json_to_file(
-        //         id,
-        //         width_r,
-        //         height_r,
-        //         base_dir,
-        //         file_name,
-        //         extension,
-        //         artist_results,
-        //         album_results,
-        //         fsize_results,
-        //         // b64image,
-        //         fullpath.to_string(),
-        //         index.to_string(),
-        //         thumb_path,
-        //     );
+            let music_image_info = MusicImageInfo {
+                id: id,
+                width: width_r,
+                height: height_r,
+                basedir: base_dir,
+                filename: file_name,
+                extension: ext,
+                artist: artist_results,
+                album: album_results,
+                filesize: fsize_results,
+                fullpath: full_path.to_string(),
+                thumbpath: thumb_path
+            };
+
+            println!("{:?}", music_image_info);
+            // crate::setup::fire_image::write_image_json_to_file(
+            //     id,
+            //     width_r,
+            //     height_r,
+            //     base_dir,
+            //     file_name,
+            //     extension,
+            //     artist_results,
+            //     album_results,
+            //     fsize_results,
+            //     // b64image,
+            //     fullpath.to_string(),
+            //     index.to_string(),
+            //     thumb_path,
+            // );
             
         };
         // put it in a db
