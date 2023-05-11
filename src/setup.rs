3f5fe330f5cf597f86e0ad3fbd2778035ae7a747
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-11 04:50:25 -0700

    test

--- src/setup.rs
+++ src/setup.rs
@@ -116,7 +116,7 @@ pub fn run_setup() -> bool {
         let media_lists = fire_walk_dirs::scan_all_sources();
 
         // run_music_threads(media_lists.0.clone());
-        // run_video_img_threads(media_lists.2.clone()); //needs work
+        run_video_img_threads(media_lists.2.clone()); //needs work
         run_music_img_threads(media_lists.2.clone());
 
         // let video_list = media_lists.1.clone();
