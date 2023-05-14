70efc2d78b52153bd089bf2319840844e1964c34
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-14 02:02:47 -0700

    insertdb test

--- src/setup/fire_process_tvshows.rs
+++ src/setup/fire_process_tvshows.rs
@@ -1,5 +1,40 @@
-use json::object;
-use std::env;
+// use json::object;
+// use std::env;
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use diesel::insert_into;
+
+mod schema {
+    diesel::table! {
+        tvshowz {
+            id -> Integer,
+        fireid -> Text,
+        index -> Text,
+        category -> Text,
+        name -> Text,
+        season -> Text,
+        episode -> Text,
+        size -> Text,
+        httppath -> Text,
+        }
+    }
+}
+
+// use schema::tvshowz;
+
+#[derive(Queryable, PartialEq, Debug)]
+struct TVShow {
+    id: i32,
+    fireid: String,
+    index: String,
+    category: String,
+    name: String,
+    season: String,
+    episode: String,
+    size: String,
+    httppath: String,
+}
+
 pub struct TVShowsUtils {
     apath: String
 }
@@ -63,7 +98,11 @@ impl TVShowsUtils {
 
 }
 
+
+
 pub fn process_tvshows(tvshows_vec: Vec<String>) -> String{
+    
+
     let mut count = 0;
     for tv in tvshows_vec {
         if tv.contains("TVShows") {
@@ -79,8 +118,8 @@ pub fn process_tvshows(tvshows_vec: Vec<String>) -> String{
             let file_size = crate::setup::fire_utils::FireUtils::get_file_size(&tvshows);
             let catagory = crate::setup::fire_process_tvshows::TVShowsUtils::get_tv_catagory(&tvshows2);
             let es = crate::setup::fire_process_tvshows::TVShowsUtils::get_tv_episode_season(&tvshows2);
-            let season = es.0;
-            let episode = es.1;
+            
+            
 
             let fire_id = crate::setup::fire_utils::FireUtils::get_md5(&tvshows);
 
@@ -90,36 +129,101 @@ pub fn process_tvshows(tvshows_vec: Vec<String>) -> String{
             for f in fnsplit {
                 fnsplit_vec.push(f.clone());
             };
-            let episodename = fnsplit_vec.pop();
+            let episodename = fnsplit_vec.pop().unwrap();
 
 
 
-            let tvshows_obj = object! {
+            // let tvshows_obj = object! {
                 
-                fireid: fire_id,
-                index: count.to_string(),
-                catagory: catagory,
-                name: episodename,
-                season: season,
-                episode: episode,
-                size: file_size,
-                httppath: tv
-            };
+            //     fireid: fire_id,
+            //     index: count.clone().to_string(),
+            //     catagory: catagory.clone(),
+            //     name: episodename.clone(),
+            //     season: es.0,
+            //     episode: es.1,
+            //     size: file_size,
+            //     httppath: tv
+            // };
 
-            let tvshows_info = json::stringify(tvshows_obj.dump());
+            // let tvshows_info = json::stringify(tvshows_obj.dump());
 
-            println!("{:?}", tvshows_info);
+            // println!("{:?}", tvshows_info);
 
-            let fire_nfo_path =
-                env::var("FIRE_NFOS").expect("$FIRE_NFOS is not set");
+            // let fire_nfo_path =
+            //     env::var("FIRE_NFOS").expect("$FIRE_NFOS is not set");
 
-            let a = format!("{}/", fire_nfo_path.as_str());
-            let b = format!("TVShows_{}_Meta.json", count.to_string());
-            let outpath = a + &b;
-            println!("\n\nthis is outpath {}\n\n", outpath);
+            // let a = format!("{}/", fire_nfo_path.as_str());
+            // let b = format!("TVShows_{}_Meta.json", count.to_string());
+            // let outpath = a + &b;
 
-            std::fs::write(outpath, tvshows_info).unwrap();
+            // std::fs::write(outpath, tvshows_info).unwrap();
+
+            // let tvs = TVShow {
+            //     id: count.clone(),
+            //     fireid: fire_id,
+            //     index: count.clone().to_string(),
+            //     category: catagory.clone(),
+            //     name: episodename.to_string(),
+            //     season: es.0,
+            //     episode: es.1,
+            //     size: file_size.clone(),
+            //     httppath: tv.clone(),
+            // };
+
+            
+            let mut conn = establish_connection();
+            
+            use schema::tvshowz::dsl::*;
+
+            let values = &vec! [
+                (
+                    id.eq(count.clone()),
+                    fireid.eq(fire_id),
+                    index.eq(count.clone().to_string()),
+                    category.eq(catagory.clone()),
+                    name.eq(episodename.to_string()),
+                    season.eq(es.0),
+                    episode.eq(es.1),
+                    size.eq(file_size.clone()),
+                    httppath.eq(tv.clone()),
+                )
+            ];
+
+            insert_into(tvshowz).values(values)
+                .execute(&mut conn);
+            
         }
     }
     count.to_string()
 }
+
+
+fn establish_connection() -> SqliteConnection {
+    let url = ::std::env::var("DATABASE_URL").unwrap();
+    SqliteConnection::establish(&url).unwrap()
+}
+
+
+
+
+
+// fn insert_tvshows(conn: &mut SqliteConnection, fireid: String, index: String, category: String, name: String, season: String, episode: String, size: String, httppath: String) -> QueryResult<()> {
+//     use diesel::dsl::insert_into;
+//     use schema::tvshowz::dsl::*;
+
+//     let tvs = TVShow {
+//         id: index,
+//         fireid: fireid,
+//         index: index,
+//         category: category,
+//         name: name,
+//         season: season,
+//         episode: episode,
+//         size: size,
+//         httppath: httppath,
+//     };
+
+//     insert_into(tvshowz)
+//         .values(&tvs)
+//         .execute(conn)
+// }
