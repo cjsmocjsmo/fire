37a0837603d06a9a8350a427a103b9ae0a1ee7b8
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-17 22:58:41 -0700

    update

--- src/setup.rs
+++ src/setup.rs
@@ -134,9 +134,9 @@ pub fn run_setup(client: Client) -> bool {
     if scan_home_dir == "yes" {
         let media_lists = fire_walk_dirs::scan_all_sources();
 
-        run_music_threads(client.clone(), media_lists.0.clone());
-        // run_music_img_threads(media_lists.2.clone());
-        // run_video_img_threads(media_lists.2.clone()); //needs work
+        // run_music_threads(client.clone(), media_lists.0.clone());
+        run_music_img_threads(media_lists.2.clone());
+        run_video_img_threads(media_lists.2.clone()); //needs work
         
 
         
