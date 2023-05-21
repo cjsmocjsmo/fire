7416da888d8e8d7700ee70b9556c2578c96979d9
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-20 21:04:21 -0700

    added server

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::time::Instant;
 use dotenvy::dotenv;
 
 pub mod setup;
+pub mod server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let client = Client::with_uri_str("mongodb://localhost:27017/fire").await?;
-    // let client = Client::with_uri_str("mongodb://db:27017/firedb").await?;
     
     let start = Instant::now();
     dotenv().ok();
     let setup_result = setup::run_setup();
+    if setup_result {
+        let _start_server = crate::server::fire_server_main();
+    };
     let duration = start.elapsed();
     println!("\nSetup is Complete! {:?} {}", duration, setup_result);
 
