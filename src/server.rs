16e9a6039540a0328eb09ebf7bcb7ca26210ae6e
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-05-22 00:42:08 -0700

    refactor server

--- src/server.rs
+++ src/server.rs
@@ -1,30 +1,32 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use std::env;
 use actix_files as fs;
 
+pub mod server_functions;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
 
 #[actix_web::main]
 pub async fn fire_server_main() -> std::io::Result<()> {
     let img_path = env::var("FIRE_THUMBNAILS").unwrap();
     HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(crate::server::server_functions::hello)
+            .service(crate::server::server_functions::echo)
+            .route("/hey", web::get().to(crate::server::server_functions::manual_hello))
             .service(fs::Files::new("/img", img_path.clone()).show_files_listing())
     })
     .bind(("192.168.0.61", 8080))?
---
+++ src/server/server_functions.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, post, HttpResponse, Responder};
+// use std::env;
+// use actix_files as fs;
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+pub async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
